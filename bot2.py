19c728233caa70916ee86ff5a74fdfcff4c26778
yarostr <85699510+yarostr@users.noreply.github.com>
2024-12-05 19:20:10 +0300

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -1,28 +1,24 @@
 from telethon import TelegramClient
-import os
 
+# Ваши данные для авторизации
 api_id = 29733643
 api_hash = 'fe6374e0aa4792328113106ac12a8bed'
 session_name = 'my_session'
 
+# Номер телефона, который вы хотите использовать для авторизации
+phone = '+79123456789'
+
 client = TelegramClient(session_name, api_id, api_hash)
 
 async def main():
-    phone = input("Введите номер телефона в формате +<7><9873000294>: ")
+    # Авторизация с заданным номером телефона
+    await client.start(phone=phone)
+    print("Авторизация успешна!")
 
-    # Попытка авторизации
-    await client.start(phone=lambda: phone)
-
-    # Если код подтверждения задан в переменной окружения, он будет автоматически использован
-    code = os.getenv('TELEGRAM_CODE')
-    if code:
-        await client.sign_in(code=code)
-        print("Авторизация завершена автоматически с использованием кода.")
-    else:
-        print("Не найден код подтверждения в переменных окружения. Пожалуйста, введите его вручную.")
-
-    # Запрос информации о забаненных пользователях
+    # Запрос информации о группе
     group_link = input("Введите ссылку на группу или её юзернейм: ")
+
+    # Пример функции для получения пользователей в ЧС
     banned_users = await get_banned_users(group_link)
     
     if banned_users:
