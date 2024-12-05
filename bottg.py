ae97b3967b4aa3af2c6cd821299bb8e25309091f
yarostr <85699510+yarostr@users.noreply.github.com>
2024-12-05 14:28:31 +0300

    Update bottg.py

--- bottg.py
+++ bottg.py
@@ -1,5 +1,9 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
 # Ваш токен
-BOT_TOKEN = os.getenv("7411390045:AAEU9UqxnwRexaIvXO4bTl4yMZkvkik75Gw")
+BOT_TOKEN = "7411390045:AAEU9UqxnwRexaIvXO4bTl4yMZkvkik75Gw"
 
 async def unban_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
@@ -9,14 +13,14 @@ async def unban_all(update: Update, context: ContextTypes.DEFAULT_TYPE):
         if user.status == "kicked":
             await context.bot.unban_chat_member(chat_id, user.user.id)
     
-    await update.message.reply_text("Все заблокированные пользователи разблокированы.")
+    await update.message.reply_text("Все заблокированные пользователи были разблокированы.")
 
 if __name__ == "__main__":
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("unbanall", unban_all))
 
-    # Получаем публичный URL из переменных среды, которые настроены на Railway
-    WEBHOOK_URL = f"https://{os.environ.get('bottg-production-33d1.up.railway.app')}/bot{BOT_TOKEN}"
+    # Получаем публичный URL вашего проекта на Railway (например, bottg.up.railway.app)
+    WEBHOOK_URL = "https://bottg.up.railway.app/bot" + BOT_TOKEN
 
     app.run_webhook(
         listen="0.0.0.0",
